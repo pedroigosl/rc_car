cdcf48fdced050a4223a927ae1c70501c7027b34
pedroigo001 <pedroigo001@alu.ufc.br>
2020-02-12 15:27:24 -0300

    identation fixedfixed identation

--- rc_car_v2.ino
+++ rc_car_v2.ino
@@ -24,21 +24,24 @@ double fb; // Forward / Back
 double ts; // Turn style
 
 //Receiver input assignment
-void setup() {
+void setup() 
+{
   pinMode(14, OUTPUT); pinMode(17, INPUT); pinMode(18, INPUT); pinMode(19, INPUT);
   Serial.begin(9600); 
 }
 
 
 
-void loop() {
+void loop() 
+{
   // input receiving (PWM reading)
   // OBS: Pin A0 / 14 has weak solder and keeps losing contact
   lr = pulseIn(17, HIGH);
   fb = pulseIn(18, HIGH);
   ts = pulseIn(19, HIGH);
 
-  if (lr > 1){
+  if (lr > 1)
+  {
     Movement();
   }
   
@@ -53,79 +56,93 @@ void loop() {
   
 }
 
-double top (double a, double b){
-    if (a > b){
-      return a;
-    }
-    return b;
+double top (double a, double b)
+{
+  if (a > b)
+  {
+    return a;
   }
+  return b;
+}
 
-void Movement (){
-
-  if (fb > 1550) { // Moves FORWARD. Bigger than positive threshold
-   
+void Movement ()
+{
+  if (fb > 1550) // Moves FORWARD. Bigger than positive threshold  
+  {
     vl = vlf * ((fb - 1550)/(1990 - 1550));
     vr = vrf * ((fb - 1550)/(1990 - 1550));
-    if (lr < 1450){
+    if (lr < 1450)
+    {
       vl = top ((vl - (vlf * ((1450 - lr)/(1450 - 990)))), 0);
     }
-    if (lr > 1550){
+    if (lr > 1550)
+    {
       vr = top ((vr - (vrf * ((lr - 1550)/(1990 - 1550)))), 0);
     }
     motorL.setSpeed(vl);
     motorL.run(FORWARD);
     motorR.setSpeed(vr);
     motorR.run(FORWARD);
-    
-  } else if (fb < 1450){ // Moves BACKWARD. Lower than positive threshold
-
-      vl = vlf * ((1450 - fb)/(1450 - 990));
-      vr = vrf * ((1450 - fb)/(1450 - 990));
-      if (lr < 1450){
-        vl = top ((vl - (vlf * ((1450 - lr)/(1450 - 990)))), 0);
-      }
-      if (lr > 1550){
-        vr = top ((vr - (vrf * ((lr - 1550)/(1990 - 1550)))), 0);
-      }
+  } 
+  else if (fb < 1450)  // Moves BACKWARD. Lower than positive threshold
+  {
+    vl = vlf * ((1450 - fb)/(1450 - 990));
+    vr = vrf * ((1450 - fb)/(1450 - 990));
+    if (lr < 1450)
+    {
+      vl = top ((vl - (vlf * ((1450 - lr)/(1450 - 990)))), 0);
+    }
+    if (lr > 1550)
+    {
+      vr = top ((vr - (vrf * ((lr - 1550)/(1990 - 1550)))), 0);
+    }
+    motorL.setSpeed(vl);
+    motorL.run(BACKWARD);
+    motorR.setSpeed(vr);
+    motorR.run(BACKWARD);
+  } 
+  else  // Stops 
+  { 
+    if (ts < 1450)  // Normal mode
+    { 
+      analogWrite(ledPin, 0);  // Set eyes to not glow
+      vl = 0;
+      vr = 0;
       motorL.setSpeed(vl);
-      motorL.run(BACKWARD);
+      motorL.run(FORWARD);
       motorR.setSpeed(vr);
-      motorR.run(BACKWARD);
-
-    } else { // Stops
-
-      if (ts < 1450) { // Normal mode
-        analogWrite(ledPin, 0);  // Set eyes to not glow
+      motorR.run(FORWARD);
+    }
+    else  // Crazy Turnz
+    {  
+      analogWrite(ledPin, 255);  // Set eyes to glow
+      if (lr < 1450)
+      {
+        vl = vlf * ((1450 - lr)/(1450 - 990));
+        vr = vrf * ((1450 - lr)/(1450 - 990));
+        motorL.setSpeed(vl);
+        motorL.run(BACKWARD);
+        motorR.setSpeed(vr);
+        motorR.run(FORWARD);
+      } 
+      else if (lr > 1550)
+      {
+        vl = vlf * ((lr - 1550)/(1990 - 1550));
+        vr = vrf * ((lr - 1550)/(1990 - 1550));
+        motorL.setSpeed(vl);
+        motorL.run(FORWARD);
+        motorR.setSpeed(vr);
+        motorR.run(BACKWARD);
+      } 
+      else 
+      {
         vl = 0;
         vr = 0;
         motorL.setSpeed(vl);
         motorL.run(FORWARD);
         motorR.setSpeed(vr);
         motorR.run(FORWARD);
-      } else{  // Crazy Turnz
-        analogWrite(ledPin, 255);  // Set eyes to glow
-        if (lr < 1450){
-          vl = vlf * ((1450 - lr)/(1450 - 990));
-          vr = vrf * ((1450 - lr)/(1450 - 990));
-          motorL.setSpeed(vl);
-          motorL.run(BACKWARD);
-          motorR.setSpeed(vr);
-          motorR.run(FORWARD);
-        } else if (lr > 1550){
-          vl = vlf * ((lr - 1550)/(1990 - 1550));
-          vr = vrf * ((lr - 1550)/(1990 - 1550));
-          motorL.setSpeed(vl);
-          motorL.run(FORWARD);
-          motorR.setSpeed(vr);
-          motorR.run(BACKWARD);
-        } else {
-          vl = 0;
-          vr = 0;
-          motorL.setSpeed(vl);
-          motorL.run(FORWARD);
-          motorR.setSpeed(vr);
-          motorR.run(FORWARD);
-        }
       }
     }
+  }
 }
